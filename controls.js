0ec926ae7595feeb606cadb2fde7fce830e6b140
unknown <andrew@elementalsystems.co.za>
2015-09-11 11:39:01 +0200

    testing mobile

--- controls.js
+++ controls.js
@@ -55,8 +55,8 @@ var controls={
 function bindControls(el) {
   el.onkeydown=function(evt) { controls.keyEvent(evt.keyCode,1);  };
   el.onkeyup=function(evt) { controls.keyEvent(evt.keyCode,0);  };
-  el.ontouchstart=function(evt) { evt.preventDefault(); controls.touch(evt.changedTouches,1); return false; }
-  el.ontouchend=function(evt) { evt.preventDefault(); controls.touch(evt.changedTouches,0); return false; }
+  //el.ontouchstart=function(evt) { evt.preventDefault(); controls.touch(evt.changedTouches,1); return false; }
+  //el.ontouchend=function(evt) { evt.preventDefault(); controls.touch(evt.changedTouches,0); return false; }
   controls.width=window.innerWidth;
   el.focus();
   
